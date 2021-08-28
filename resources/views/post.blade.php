23c92a3e28a6b0557c851973a97d6a8d12043f9d
felipebat20 <felipebatista24680@gmail.com>
2021-08-28 17:52:43 -0300

    Aula 10

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,11 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Minha página</title>
-    <link rel="stylesheet" href="./app.css">
+    <link rel="stylesheet" href="../app.css">
 </head>
 <body>
     <article>
         <?= $post ?>
+
     </article>
 
     <a href="/">Go back</a>
